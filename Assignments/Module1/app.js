1243832703b368022fa2720db9df03cf84202030
JinxDarkie <godmachineinc@gmail.com>
2023-11-10 18:44:40 +0800

    ...

--- Assignments/Module1/app.js
+++ Assignments/Module1/app.js
@@ -1,28 +1,33 @@
+/**
+ * TODO: Implement basic result logic.
+ *          itemTick = 0 : "Please enter data first"
+ *          0 < itemTick <= 3 : "Enjoy!"
+ *          3 < itemTick: "Too much!"
+ */
+
 (function (){ 
-    /*Remember, you need this *immediately invoked function expression* to 
-ensure variable scope stays in this function instead of going global and 
-wasting resources. The angular.min file uses it too.*/
-
     'use strict'; 
-    //Adds additional protections, much like the above IIFE
-    //In this case I believe it's mainly enabling *strict* typing, so you can't
-    //just 'x="yay";' a new var it needs to be defined as such.
 
-    angular.module('firstAppTest', [])
-    //The empty [] contains the module's dependencies.
+    angular
+        .module('lunchChecker', [])
+        .controller('lunchCheckController', lunchCheckController);
 
-    /*$scope is akin to props from react.js, except that it allows any
-    variables you attach to it to be accessible to whatever element the
-    .controller() is bound to (In this case 'controllerExample'). The
-    children of that element can then access that data.*/
-    .controller('controllerExample', function ($scope) {
-        $scope.sampleText = "hello world!";
+    function lunchCheckController ($scope, $filter) {
+        $scope.lunchList = "";
+        $scope.outputMessage = "";
 
-        $scope.inputText = "";
-        $scope.inputLength= 0;
-        $scope.displayLength = function(){
-            $scope.inputLength = $scope.inputText.length;
+        $scope.processList = function (){
+            var splitLunchList = $scope.lunchList.split(',');
+            var itemTick = 0;
+
+            splitLunchList.forEach(menuItem => {
+                if(menuItem!=""){
+                    itemTick++;
+                }
+            });
+
+            $scope.outputMessage="Testing testing";
         }
-    });
+    }
 
 })();
